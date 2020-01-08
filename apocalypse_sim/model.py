6418460d279717623cb66685d731c961a2ec9ef6
Ricardo van Aken <rvaken98@gmail.com>
2020-01-08 12:45:29 +0100

    agents are now added, they dont yet have an AI

---
+++ apocalypse_sim/model.py
@@ -0,0 +1,56 @@
+"""model.py
+
+Creates the model for the computational experiment that needs to run.
+"""
+from mesa import Model, Agent
+from mesa.time import RandomActivation
+from mesa.space import SingleGrid
+from mesa.datacollection import DataCollector
+
+class ApocalypseAgent(Agent):
+    def __init__(self, pos, model, type, properties):
+        super().__init__(pos, model)
+        self.type = type
+        self.pos = pos
+
+    def step(self):
+        self.model.grid.move_to_empty(self)
+
+
+class Apocalypse(Model):
+    def __init__(self, height=200, width=200, density=0.8, percent_infected=0.1):
+        self.height = height
+        self.width = width
+        self.density = density
+        self.percent_infected = percent_infected
+
+        self.schedule = RandomActivation(self)
+        self.grid = SingleGrid(height, width, torus=True)
+
+        self.datacollector = DataCollector(
+            {"type": "zombie"},  # Model-level count of zombie agents
+            # For testing purposes, agent's individual x and y
+            {"x": lambda a: a.pos[0], "y": lambda a: a.pos[1]})
+
+        for cell in self.grid.coord_iter():
+            x = cell[1]
+            y = cell[2]
+            if self.random.random() < self.density:
+                if self.random.random() < self.percent_infected:
+                    type = "zombie"
+                else:
+                    type = "human"
+
+                new_agent = ApocalypseAgent((x, y), self, type, {"strength" : 1})
+
+                self.grid.position_agent(new_agent, (x, y))
+                self.schedule.add(new_agent)
+
+        # no idea what this does
+        self.running = True
+        self.datacollector.collect(self)
+
+
+    def step(self):
+        self.schedule.step()
+        self.datacollector.collect(self)
---
+++ apocalypse_sim/run.py
@@ -0,0 +1,7 @@
+"""run.py
+
+Starts server to run computational model using "Mesa".
+"""
+from server import server
+
+server.launch()
