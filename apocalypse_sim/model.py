356fef899741698f2a44e5922cb42825cfca3780
Pim Paardekooper <pimpaardekooper@me.com>
2020-01-08 17:34:43 +0100

    second map with places and roades

--- apocalypse_sim/model.py
+++ apocalypse_sim/model.py
@@ -7,28 +7,31 @@ from mesa.time import RandomActivation
 from mesa.space import SingleGrid
 from mesa.datacollection import DataCollector
 
+from matplotlib.path import Path
+
 class ApocalypseAgent(Agent):
-    def __init__(self, pos, model, type, properties):
+    def __init__(self, pos, model, agent_type, properties={}):
         super().__init__(pos, model)
-        self.type = type
+        self.type = agent_type
         self.pos = pos
         self.properties = properties
 
     def step(self):
         # get neighbours within vision(later make vision a property of an agent?)
         vision = 5;
-        neighbours = self.model.grid.get_neighbors(self.pos, False, radius=self.properties["vision"])
+        neighbours = self.model.grid.get_neighbors(
+            self.pos, False, radius=self.properties["vision"])
 
         # print the neighbours for debugging purposes
         neigh = ""
         for n in neighbours:
             neigh += "(" + str(n.pos) + ", " + self.type + "), "
-        print("(" + str(self.pos) + ", " + self.type + ") " + "neighbours: " + neigh)
+
+        # print("(" + str(self.pos) + ", " + self.type + ") " + "neighbours: " + neigh)
 
         # until an AI is implemented, move the agent to a completely random
         # square in the grid
-        self.model.grid.move_to_empty(self)
-
+        # self.model.grid.move_to_empty(self)
 
 class Apocalypse(Model):
     def __init__(self, height=100, width=100, density=0.1, infected=0.05):
@@ -37,7 +40,6 @@ class Apocalypse(Model):
         self.width = width
         self.density = density
         self.infected = infected
-
 
         # NOTE: no idea what this does
         self.schedule = RandomActivation(self)
@@ -49,31 +51,99 @@ class Apocalypse(Model):
             {"x": lambda a: a.pos[0], "y": lambda a: a.pos[1]})
         # NOTE: end of weird stuff
 
-
         # All agents are created here
-        for cell in self.grid.coord_iter():
-            x = cell[1]
-            y = cell[2]
-            if self.random.random() < self.density:
-                properties = {}
-                if self.random.random() < self.infected:
-                    properties["vision"] = 4
-                    type = "zombie"
-                else:
-                    properties["vision"] = 5
-                    type = "human"
-
-                new_agent = ApocalypseAgent((x, y), self, type, properties=properties)
-                self.grid.position_agent(new_agent, (x, y))
-                self.schedule.add(new_agent)
-
+        # self.initial_map()
+        self.second_map()
 
         # NOTE: no idea what this does
         self.running = True
         self.datacollector.collect(self)
         # NOTE: end of weird stuff
 
-
     def step(self):
         self.schedule.step()
         self.datacollector.collect(self)
+
+    def initial_map(self):
+        for cell in self.grid.coord_iter():
+            x = cell[1]
+            y = cell[2]
+
+            if self.random.random() < self.density:
+                properties = {}
+                if self.random.random() < self.infected:
+                    properties["vision"] = 4
+                    agent_type = "zombie"
+                else:
+                    properties["vision"] = 5
+                    agent_type = "human"
+
+                new_agent = ApocalypseAgent((x, y), self, agent_type, properties=properties)
+                self.grid.position_agent(new_agent, (x, y))
+                self.schedule.add(new_agent)
+            else:
+                agent_type = 0
+                agent = ApocalypseAgent((x, y), self, agent_type, properties={})
+                self.grid.position_agent(agent, (x, y))
+                self.schedule.add(agent)
+
+
+    def second_map(self):
+        """
+        Map that has a city and village and a road between. Every point that is
+        not inside it you can't walk.
+        """
+        city = Place(0.3, [[50, 75],
+                           [75, 75],
+                           [75,50],
+                           [50,50],
+                           [50, 75]])
+
+        village = Place(0.1, [[5,5],[10,5],[10,10],[5, 10],[5,5]])
+        road = Place(0, [[8, 10] , [10, 8], [52, 50], [50, 52], [8, 10]])
+
+        for cell in self.grid.coord_iter():
+            x = cell[1]
+            y = cell[2]
+
+            added = False
+
+            for place in [city, village]:
+                if place.path.contains_point((x, y)):
+                    added = True
+
+                    if self.random.random() < place.population_density:
+                        properties = {}
+                        if self.random.random() < self.infected:
+                            properties["vision"] = 4
+                            agent_type = "zombie"
+                        else:
+                            properties["vision"] = 5
+                            agent_type = "human"
+
+                        new_agent = ApocalypseAgent((x, y), self, agent_type, properties=properties)
+                        self.grid.position_agent(new_agent, x, y)
+                        self.schedule.add(new_agent)
+
+
+            if not added:
+                for r in [road]:
+                    if r.path.contains_point((x, y), radius=1):
+                        added = True
+                        break
+
+            if not added:
+                new_agent = ApocalypseAgent((x, y), self, "wall", properties={})
+                self.grid.position_agent(new_agent, x, y)
+                self.schedule.add(new_agent)
+
+
+
+
+class Place:
+    def __init__(self, population_density, vertices):
+        self.population_density = population_density
+        self.path = Path(vertices)
+
+    # def inside_square(self, x, y):
+        # return x >= self.x1 and x <= self.x2 and y >= self.y1 and y <= self.y2
--- apocalypse_sim/server.py
+++ apocalypse_sim/server.py
@@ -15,6 +15,7 @@ def model_draw(agent):
     '''
     if agent is None:
         return
+
     portrayal = {"Shape": "circle", "r": 1, "Filled": "true", "Layer": 0}
 
     if agent.type == "zombie":
@@ -23,16 +24,23 @@ def model_draw(agent):
     elif agent.type == "human":
         portrayal["Color"] = ["#0000FF", "#9999FF"]
         portrayal["stroke_color"] = "#000000"
+    else:
+        portrayal["Color"] = ["#000000", "#000000"]
+        portrayal["stroke_color"] = "#000000"
+
     return portrayal
 
-canvas_element = CanvasGrid(model_draw, 100, 100, 1000, 1000)
+grid_height = 100
+grid_width = 100
+
+canvas_element = CanvasGrid(model_draw, grid_height, grid_width, 800, 800)
 # happy_chart = ChartModule([{"Label": "happy", "Color": "Black"}])
 
 
 # NOTE: Add sliders here
 model_params = {
-    "height": 100,
-    "width": 100,
+    "height": grid_height,
+    "width": grid_width,
     "density": UserSettableParameter("slider", "Agent density", 0.1, 0.01, 1.0, 0.01),
     "infected": UserSettableParameter("slider", "Amount infected", 0.1, 0.01, 1.0, 0.01)
 }
