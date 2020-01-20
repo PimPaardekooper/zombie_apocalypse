691804fc333409d9aef4850d67733f2463e10d69
Mund Vetter <mundvetter@gmail.com>
2020-01-20 12:32:28 +0100

    added new tests

--- apocalypse_sim/agents/map_layouts.py
+++ apocalypse_sim/agents/map_layouts.py
@@ -21,7 +21,7 @@ class Map:
 
         if is_verification():
             maps = [
-               self.convert_test, self.situation_map, self.second_map, self.third_map, self.fourth_map, self.fifth_map,
+               self.convert_test, self.range_test, self.group_test, self.third_map, self.fourth_map, self.fifth_map,
                 self.sixth_map, self.situation_map            ]
 
         self.places, self.roads, self.agents = maps[map_id]()
@@ -277,6 +277,34 @@ class Map:
 
         return [city], [], [humans, zombies]
 
+    def range_test(self):
+        """Square map no walls."""
+        city = Place([[0, 0],
+                      [0, self.model.grid.height],
+                      [self.model.grid.width,
+                       self.model.grid.height],
+                      [self.model.grid.width, 0],
+                      [0, 0]],
+                     0)
+
+        humans = Agents("human", [(0, 0)])
+        zombies = Agents("zombie", [(7, 7)])
+
+        return [city], [], [humans, zombies]
+
+    def group_test(self):
+        """Square map no walls."""
+        city = Place([[0, 0],
+                      [0, self.model.grid.height],
+                      [self.model.grid.width,
+                       self.model.grid.height],
+                      [self.model.grid.width, 0],
+                      [0, 0]],
+                     0)
+
+        humans = Agents("human", [(0, 0), (9, 0), (0, 9), (9, 9)])
+
+        return [city], [], [humans]
 
 class Agents:
     def __init__(self, agent_type, positions, attr={}):
